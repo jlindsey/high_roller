6104d0e1c9f68956b8cab9313c6520e4e4bd3a1e
Josh Lindsey <josh@core-apps.com>
2013-06-30 10:13:40 -0400

    Update single run CLI

--- lib/high_roller/cli/single.rb
+++ lib/high_roller/cli/single.rb
@@ -1,11 +1,10 @@
 class HighRoller::CLI::Single
-  def self.parse argv
+  def parse input
     rgen = HighRoller::Random::Generator.new :pseudo
-
     parser = HighRoller::Parsing::Parser.instance
     
     begin
-      dice = parser.parse(argv[0].strip)
+      dice = parser.parse(input.strip)
     rescue HighRoller::Exceptions::ParseError => e
       puts "Unable to parse input: #{e.message}"
       exit 1
